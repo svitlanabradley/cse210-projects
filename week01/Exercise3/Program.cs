8ab3f88a43bde3e7bd0f1c977c8afba83ee0c15f
svitlanabradley <svitlana.bradley@gmail.com>
2025-05-06 19:03:27 +0200

    completed Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string again = "y";
+        while (again == "y")
+        {
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1, 101);
+            int guessNum = -1;
+            int tries = 0;
+    
+            while (magicNum != guessNum)
+            { 
+                Console.Write("What is your guess? ");
+                guessNum = int.Parse(Console.ReadLine());
+                tries = tries + 1;
+    
+                if (magicNum < guessNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if ( magicNum > guessNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You quessed it!");
+                }
+            }
+            Console.WriteLine($"It took you {tries} guesses.");
+
+            Console.Write("Do you want to play again? (y/n) ");
+            again = Console.ReadLine();
+        }
+        Console.Write("Goodbye");
     }
 }
